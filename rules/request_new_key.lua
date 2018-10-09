53f14adc5ab6c18fbb2d07cf05f80526df5a7ea2
Mitchell Tannenbaum <tannenbaum.mitchell@gmail.com>
2018-10-09 16:18:05 -0700

    Cleaning

--- rules/request_new_key.lua
+++ rules/request_new_key.lua
@@ -1,3 +1,7 @@
-if req.method == "GET" and req.path:match("/get_keys/?") then
+if
+	req.method == "GET"
+	and
+	req.path:match("/get_keys/?")
+	then
     events["key_generation_request"]:trigger(req)
 end
