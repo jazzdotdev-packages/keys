b5695dacd3e392012e4b54aabd70c2b330c47929
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-11-26 16:06:32 -0400

    Warn sign error

--- actions/sign_outgoing_response.lua
+++ actions/sign_outgoing_response.lua
@@ -2,8 +2,8 @@ event: ["outgoing_response_about_to_be_sent"]
 priority: 2 -- Before logging the response
 input_parameters: ["response"]
 
-local signed = keys.sign_http_message(response)
+local signed, err = keys.sign_http_message(response)
 
 if not signed then
-  log.error("Could not sign response")
+  log.warn("Could not sign response: " .. err)
 end
