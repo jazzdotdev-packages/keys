b405ad485e5c1a925c883e40de2ede4dab17aa3a
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2019-02-10 08:49:41 -0500

    Use contentdb

--- actions/create_profile.lua
+++ actions/create_profile.lua
@@ -3,7 +3,7 @@ priority = 1
 input_parameters = ["request"]
 
 
-local profile = content.walk_documents(content.home,
+local profile = contentdb.walk_documents(contentdb.home,
   function (file_uuid, header, body)
     if header.type == "profile" then
       return {name = header.name, uuid = file_uuid}
@@ -12,7 +12,7 @@ local profile = content.walk_documents(content.home,
 )
 
 if profile then
-  local path = content.stores[content.home] .. profile.uuid
+  local path = contentdb.stores[contentdb.home] .. profile.uuid
   local msg = "A profile '" .. profile.name .. "' already exists in '" .. path .. "'"
   log.warn(msg)
   return {
@@ -23,25 +23,25 @@ if profile then
   }
 end
 
-local profile_uuid = content.home
+local profile_uuid = contentdb.home
 
 local sign_priv, sign_pub = crypto.sign.new_keypair()
 
 local sign_priv_id = uuid.v4()
 local sign_pub_id = uuid.v4()
 
-content.write_file(content.home, profile_uuid, {
+contentdb.write_file(contentdb.home, profile_uuid, {
   model = "profile",
   name = request.body["fields.name"]
 })
 
-content.write_file(content.home, sign_priv_id, {
+contentdb.write_file(contentdb.home, sign_priv_id, {
   model = "key",
   kind = "sign_private",
   profile_uuid = profile_uuid,
 }, tostring(sign_priv))
 
-content.write_file(content.home, sign_pub_id, {
+contentdb.write_file(contentdb.home, sign_pub_id, {
   model = "key",
   kind = "sign_public",
   private_uuid = sign_priv_id,
